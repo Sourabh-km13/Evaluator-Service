407c0ff8303959f704da308600f4daa5a76e8b96
sourabh-km13 <157295892+Sourabh-km13@users.noreply.github.com>
2025-07-30 17:01:10 +0530

    integrated Bull mq Ui for queue management

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,22 @@ import { Port } from "./config/index";
 import apiRouter from "./routes/index";
 import sampleQueueProducer from "./producer/Sample.producer";
 import SampleWorker from "./worker/Sample.worker";
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { ExpressAdapter } from "@bull-board/express";
+import SampleQueue from "./queues/Sample.queue";
 
-
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/admin/queues");
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: [new BullMQAdapter(SampleQueue)],
+  serverAdapter: serverAdapter,
+});
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/admin/queues", serverAdapter.getRouter());
 app.use("/api", apiRouter);
 app.get("/", (req, res) => {
   console.log(req.url);
@@ -17,14 +27,13 @@ app.get("/", (req, res) => {
   });
 });
 
-
 app.listen(Port, () => {
   console.log("server running on port:", Port);
-  SampleWorker('SampleQueue')
-  sampleQueueProducer("SampleJob",{
-    name:"Sourabh",
-    company:"Hack2skill",
-    postion:"Intern",
-    location:"Noida"
-  })
+  SampleWorker("SampleQueue");
+  sampleQueueProducer("SampleJob", {
+    name: "Sourabh",
+    company: "Hack2skill",
+    postion: "Intern",
+    location: "Noida",
+  });
 });
